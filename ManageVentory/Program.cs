82c2cc7781dc4f05bca9fee7f1d2e024a12d40ba
Nirajan <nirajanshrestha672@gmail.com>
2024-12-17 10:21:31 -0500

    added wwwroot folder and public files

--- ManageVentory/Program.cs
+++ ManageVentory/Program.cs
@@ -5,6 +5,10 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+//Allows progarm to use public files in wwwroot
+app.UseStaticFiles();
+
+//Alows program to use routing
 app.UseRouting();
 
 //Route taken from the controller where controller name is Home and action is index
