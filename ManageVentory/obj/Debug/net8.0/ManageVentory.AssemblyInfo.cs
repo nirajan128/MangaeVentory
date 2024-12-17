a59da525494831fae7b615e2f67f93eb68e0dccd
Nirajan <nirajanshrestha672@gmail.com>
2024-12-17 11:41:27 -0500

    added section to the views

--- ManageVentory/obj/Debug/net8.0/ManageVentory.AssemblyInfo.cs
+++ ManageVentory/obj/Debug/net8.0/ManageVentory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManageVentory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c775a1b11eeb08b7fdf1946f6181aae4057a36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82c2cc7781dc4f05bca9fee7f1d2e024a12d40ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManageVentory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManageVentory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
